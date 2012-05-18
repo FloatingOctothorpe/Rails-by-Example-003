ac4f9bbea2f832fea2796bf8cf9e33e3171fad2c
Andrew Hounsell <andrewhounsell@gmail.com>
2012-05-18 21:48:57 +0100

    Added microposts delete link tests

--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "Microposts" do
 
   before(:each) do
-    user = Factory(:user)
+    @user = Factory(:user)
     visit signin_path
-    fill_in :email,    :with => user.email
-    fill_in :password, :with => user.password
+    fill_in :email,    :with => @user.email
+    fill_in :password, :with => @user.password
     click_button
   end
 
@@ -44,6 +44,29 @@ describe "Microposts" do
           click_button
           response.should render_template('pages/home')
           response.should have_selector("span.microposts", :content => expected_text)
+        end
+      end
+
+      it "should have a delete link for the user's post" do
+        content = "A new post with a delete button."
+        visit root_path
+        fill_in :micropost_content, :with => content
+        click_button
+        response.should have_selector("ol.microposts") do
+          have_selector("a", :href => "/users/#{@user.id}")
+          have_selector("span.content", :content => content)
+          have_selector("a", :content => "delete")
+        end
+      end
+
+      it "should not have a delete link for other user's post" do
+        content = "A new post without a delete button."
+        wrong_user = Factory(:user, :email => "wronguser@example.com")
+        wrong_post = Factory(:micropost, :user => wrong_user, :content => content)
+        visit root_path
+        response.should_not have_selector("ol.microposts") do
+          have_selector("a", :href => "/users/#{wrong_user.id}")
+          have_selector("a", :content => "delete")
         end
       end
     end
